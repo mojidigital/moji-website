518c946485287c6e8251e54d848cc107b96c0b98
Tom Groves <tom@tomgroves.com>
2024-03-30 11:58:51 +0000

    initial basic landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      <div className="logo"></div>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        <h3>Hey there, we are moji digital.</h3>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          We create and develop successful web and mobile software capabilities
+          to empower and accelerate digital transformation.
+        </p>
+        <p>
+          Our team have delivered innovative and powerful solutions to some of
+          the world's leading brands, in sectors spanning financial service,
+          aviation, healthcare, manufacturing and retail.
+        </p>
+        <p>
+          We'd love to talk to you about how we can help to make your business
+          thrive in the world of digital disruption.
+        </p>
+        <p>
+          E-mail us at{" "}
+          <a href="mailto:hello@moji.digital" target="_blank" rel="noreferrer">
+            hello@moji.digital
+          </a>
+        </p>
+        <p>
+          Or call or <a href="https://wa.me/441613991787" target="_blank" rel="noreferrer">WhatsApp</a> us on <a href="tel:+441613991787" target="_blank" rel="noreferrer">+44 (0)161 399 1787</a>
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
