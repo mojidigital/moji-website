1828b0dcb36f28cd026f6e8449ebd0075d5ef98e
Tom Groves <tom@tomgroves.com>
2024-03-30 12:25:12 +0000

    add subtle circular background

--- src/App.tsx
+++ src/App.tsx
@@ -3,31 +3,48 @@ import "./App.css";
 function App() {
   return (
     <>
-      <div className="logo"></div>
-      <div className="card">
-        <h3>Hey there, we are moji digital.</h3>
-        <p>
-          We create and develop successful web and mobile software capabilities
-          to empower and accelerate digital transformation.
-        </p>
-        <p>
-          Our team have delivered innovative and powerful solutions to some of
-          the world's leading brands, in sectors spanning financial service,
-          aviation, healthcare, manufacturing and retail.
-        </p>
-        <p>
-          We'd love to talk to you about how we can help to make your business
-          thrive in the world of digital disruption.
-        </p>
-        <p>
-          E-mail us at{" "}
-          <a href="mailto:hello@moji.digital" target="_blank" rel="noreferrer">
-            hello@moji.digital
-          </a>
-        </p>
-        <p>
-          Or call or <a href="https://wa.me/441613991787" target="_blank" rel="noreferrer">WhatsApp</a> us on <a href="tel:+441613991787" target="_blank" rel="noreferrer">+44 (0)161 399 1787</a>
-        </p>
+      <div className="container">
+        <div className="logo"></div>
+        <div className="card">
+          <h3>Hey there, we are moji digital.</h3>
+          <p>
+            We create and develop successful web and mobile software
+            capabilities to empower and accelerate digital transformation.
+          </p>
+          <p>
+            Our team have delivered innovative and powerful solutions to some of
+            the world's leading brands, in sectors spanning financial service,
+            aviation, healthcare, manufacturing and retail.
+          </p>
+          <p>
+            We'd love to talk to you about how we can help to make your business
+            thrive in the world of digital disruption.
+          </p>
+          <p>
+            E-mail us at{" "}
+            <a
+              href="mailto:hello@moji.digital"
+              target="_blank"
+              rel="noreferrer"
+            >
+              hello@moji.digital
+            </a>
+          </p>
+          <p>
+            Or call or{" "}
+            <a
+              href="https://wa.me/441613991787"
+              target="_blank"
+              rel="noreferrer"
+            >
+              WhatsApp
+            </a>{" "}
+            us on{" "}
+            <a href="tel:+441613991787" target="_blank" rel="noreferrer">
+              +44 (0)161 399 1787
+            </a>
+          </p>
+        </div>
       </div>
     </>
   );
